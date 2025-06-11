527ea7748c8fb47b12683736b859e4e9ac455f9d
Gabriel Vinícius Ferreira da Silva <107225167+GabrielVinicius7@users.noreply.github.com>
2025-06-11 08:38:55 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,30 +3,30 @@ from torchvision import transforms
 from PIL import Image
 from cnn_model import SimpleCNN
 
-# Carrega as classes do arquivo classes.txt
+
 with open("classes.txt", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
-# Caminho da imagem a ser testada
-image_path = "./avaliation/test1.jpg"  # Substitua pelo caminho real da imagem
 
-# Transformações da imagem (iguais às usadas no treino)
+image_path = "./avaliation/test1.jpg"  
+
+
 transform = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
 ])
 
-# Inicializa e carrega o modelo
+
 model = SimpleCNN(num_classes=len(classes))
 model.load_state_dict(torch.load("modelo_treinado.pth"))
 model.eval()
 
-# Abre e transforma a imagem
-image = Image.open(image_path).convert("RGB")
-input_tensor = transform(image).unsqueeze(0)  # Adiciona dimensão de batch
 
-# Faz a predição
+image = Image.open(image_path).convert("RGB")
+input_tensor = transform(image).unsqueeze(0)  
+
+
 with torch.no_grad():
     output = model(input_tensor)
     _, predicted = torch.max(output, 1)
