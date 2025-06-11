01c1bb3b370f1b7a8ba193ea7e9395e32459153b
Gabriel Vinícius Ferreira da Silva <107225167+GabrielVinicius7@users.noreply.github.com>
2025-06-11 08:40:51 -0400

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -4,16 +4,16 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)   # Imagens com 3 canais (RGB)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 16 * 16, 128)  # 64 filtros, imagem final 16x16 após pooling
+        self.fc1 = nn.Linear(64 * 16 * 16, 128)  
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))  # [B, 32, 32, 32]
-        x = self.pool(torch.relu(self.conv2(x)))  # [B, 64, 16, 16]
-        x = x.view(x.size(0), -1)                 # Flatten para [B, 64*16*16]
+        x = self.pool(torch.relu(self.conv1(x))) 
+        x = self.pool(torch.relu(self.conv2(x))) 
+        x = x.view(x.size(0), -1)                
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
